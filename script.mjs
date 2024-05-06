ad9fe0937c34061338e187954f15b95ca7c4e0a2
ludevique <makamludovique@gmail.com>
2024-05-06 16:06:29 -0700

    if-statement

---
+++ script.mjs
@@ -0,0 +1,26 @@
+//part1
+//let's start by having the total capacity of the garden.
+//we have an area of 5 meters and we need to know the capacity.
+// in other to have it we should make an operation that gives us the answer.
+
+const PI = 3.1415;
+const radius = 5;
+const capacity = 3.1415 * 25
+console.log(capacity)
+console.log(`so we have a total space of ${capacity} `)
+
+//we have a total plants of 20 which takes a min of 0.8 meters
+//let's know how much space 20 plants will take accorrding to their spaces eacch
+let numPlant = 20;
+let spaceEach = 0.8
+let space = 20 / 0.64
+console.log(space)
+console.log(`20 plants will occupied ${space} of the capacity`)
+//we already know they double each week
+
+//we have a total space of 78.53 and 20 plants takes up to 31.25 of the space.therefore let's see how much we have after.
+
+let remain = 78.53 - 31.25
+console.log(remain)
+
+space = 78.537
